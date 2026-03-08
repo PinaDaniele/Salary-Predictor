8a13bd7a6fecb0b5546e4d4fc69f1653e482a058
PinaDaniele <pinazdaniele@gmail.com>
2026-03-08 18:44:37 +0100

    Added data preprocessing

--- train.py
+++ train.py
@@ -4,6 +4,10 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import joblib
 
 DATASET_PATH = ".\\dataset.csv"
 EDA_PATH = ".\\eda"
@@ -65,9 +69,49 @@ def analyze_data(df):
     sns.heatmap(matrix, annot=True, cmap="coolwarm")
     plt.savefig(os.path.join(EDA_PATH, "cat_feature_heatmap.png"))
 
+#TODO: DROP employee residence, job_category
+#log scale salary_in_usd
+#encode cat features in some way
+def preprocess_data(df):
+    salaries = df["salary_in_usd"].to_numpy()
+    Y = np.log(salaries)
+
+    df.drop(columns=["employee_residence", "job_category", "salary_in_usd"], inplace=True)
+    df["company_size"] = df["company_size"].map({"S":0, "M":1, "L":2})
+    df["experience_level"] = df["experience_level"].map({"Entry-level":0, "Mid-level":1, "Senior":2, "Executive":3})
+
+    job_title_threshold = 0.05
+    job_counts = df["job_title"].value_counts(normalize=True)
+    df["job_title"] = df["job_title"].apply(lambda x: x if job_counts[x] > job_title_threshold else "Other")
+
+    region_threshold = 0.10
+    region_counts = df["company_location"].value_counts(normalize=True)
+    df["company_location"] = df['company_location'].apply(lambda x: x if region_counts[x] > region_threshold else "Other")
+
+    year_min = df["work_year"].min()
+    df["work_year"] = df["work_year"].apply(lambda x: x-year_min)
+
+    num_cols = ["work_year", "experience_level", "company_size"]
+    cat_cols = ["job_title", "employment_type", "work_setting", "company_location"]
+
+    X_train, X_test, Y_train, Y_test = train_test_split(df, Y, test_size=0.2, random_state=42)
+    preprocessor = ColumnTransformer(transformers=[
+        ("num", StandardScaler(), num_cols),
+        ("cat", OneHotEncoder(drop="first", handle_unknown="ignore"), cat_cols)
+    ])
+
+    X_train = preprocessor.fit_transform(X_train)
+    X_test = preprocessor.transform(X_test)
+
+    return X_train, X_test, Y_train, Y_test, preprocessor
+
+
+
 def main():
     df = load_data()
     analyze_data(df)
+    X_train, X_test, Y_train, Y_test, preprocessor = preprocess_data(df)
+    joblib.dump(preprocessor, "preprocessor")
 
 
 if __name__ == "__main__":
